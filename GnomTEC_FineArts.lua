a57e667fa4fc32014e6aa3c2ffc2c4e6525057f7
GnomTEC <info@gnomtec.de>
2017-08-30 19:24:35 +0200

    * Updated TOC for WoW patch 7.3.0

--- GnomTEC_FineArts.lua
+++ GnomTEC_FineArts.lua
@@ -1,6 +1,6 @@
 -- **********************************************************************
 -- GnomTEC FineArts
--- Version: 7.2.0.1
+-- Version: 7.3.0.1
 -- Author: Peter Jack
 -- URL: http://www.gnomtec.de/
 -- **********************************************************************
@@ -28,14 +28,14 @@ local L = LibStub("AceLocale-3.0"):GetLocale("GnomTEC_FineArts")
 local addonInfo = {
 	["Name"] = "GnomTEC FineArts",
 	["Description"] = "GnomTEC FineArts.",	
-	["Version"] = "7.2.0.1",
-	["Date"] = "2017-04-01",
+	["Version"] = "7.3.0.1",
+	["Date"] = "2017-08-30",
 	["Author"] = "Peter Jack",
 	["Email"] = "info@gnomtec.de",
 	["Website"] = "http://www.gnomtec.de/",
 	["Copyright"] = "© 2017 by Peter Jack",
 	["License"] = "European Union Public Licence (EUPL v.1.1)",	
-	["FrameworkRevision"] = 9
+	["FrameworkRevision"] = 10
 }
 
 -- ----------------------------------------------------------------------
